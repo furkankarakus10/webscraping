4a68e85c03a390f4dcc68c2e848b1f93b04f6b41
Furkan Karakuş <furkan.karakus@icloud.com>
2023-12-28 11:17:00 +0300

    2

---
+++ web_scraping1.py
@@ -0,0 +1,27 @@
+import requests
+
+from bs4 import BeautifulSoup
+
+header = {
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
+url = "https://www.imdb.com/chart/top/?ref_=nv_mv_250"
+
+response = requests.get(url=url,headers=header)
+
+html_icerigi = response.content
+
+soup = BeautifulSoup(html_icerigi,"html.parser")
+
+basliklar = soup.find_all("a",{"class":"ipc-title-link-wrapper"})
+
+puanlar = soup.find_all("span",{"class":"sc-43986a27-1 fVmjht"})
+
+
+for baslik,puan in zip(basliklar,puanlar):
+    print(baslik.text,"\n",puan.text[:3])
+    print("*******************")
+
+
+
